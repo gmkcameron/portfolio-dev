090dc7b2dd8dbda62cbab9d7f673fb3c4f8f90df
Cameron <gmkcameron84@gmail.com>
2025-10-25 12:07:09 -0300

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ import {
   FaDatabase,
   FaNpm
 } from 'react-icons/fa'
-import { SiExpress, SiMongodb, SiPostgresql } from 'react-icons/si'
+import { SiExpress, SiMongodb, SiPostgresql, SiTypescript, SiAmazonaws, SiFigma } from 'react-icons/si'
 import './Skills.css'
 
 function Skills() {
@@ -20,6 +20,7 @@ function Skills() {
         { name: 'HTML5', icon: <FaHtml5 />, color: '#E34F26' },
         { name: 'CSS3', icon: <FaCss3Alt />, color: '#1572B6' },
         { name: 'JavaScript', icon: <FaJs />, color: '#F7DF1E' },
+        { name: 'TypeScript', icon: <SiTypescript />, color: '#3178C6' },
         { name: 'React', icon: <FaReact />, color: '#61DAFB' }
       ]
     },
@@ -37,7 +38,9 @@ function Skills() {
       items: [
         { name: 'Git', icon: <FaGitAlt />, color: '#F05032' },
         { name: 'NPM', icon: <FaNpm />, color: '#CB3837' },
-        { name: 'REST API', icon: <FaDatabase />, color: '#0078D4' }
+        { name: 'REST API', icon: <FaDatabase />, color: '#0078D4' },
+        { name: 'AWS', icon: <SiAmazonaws />, color: '#FF9900' },
+        { name: 'Figma', icon: <SiFigma />, color: '#A259FF' }
       ]
     }
   ]
